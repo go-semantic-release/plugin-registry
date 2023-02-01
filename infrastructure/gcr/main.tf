7365682672874a6dd2183ed905eaa744cd789203
Christoph Witzko <github@christophwitzko.com>
2023-02-01 17:51:50 +0100

    fix: use the correct deployment region

--- infrastructure/gcr/main.tf
+++ infrastructure/gcr/main.tf
@@ -66,3 +66,13 @@ resource "google_cloud_run_v2_service" "default" {
     percent = 100
   }
 }
+
+// allow public access
+resource "google_cloud_run_service_iam_binding" "default" {
+  location = google_cloud_run_v2_service.default.location
+  service  = google_cloud_run_v2_service.default.name
+  role     = "roles/run.invoker"
+  members = [
+    "allUsers"
+  ]
+}
